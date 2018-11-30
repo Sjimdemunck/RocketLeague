a069bc352aa71d17c5a631599a3813b80faa2711
vagrant <vagrant@homestead>
2018-11-30 09:56:05 +0000

    Update Seeding

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET SQL_SAFE_UPDATES=0;');
+            (new User)->delete();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            User::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        DB::statement('SET SQL_SAFE_UPDATES=1;');
+
         factory(App\User::class, 250)->create();
     }
 }
